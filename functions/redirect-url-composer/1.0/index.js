31e6702a188596c65bcfed88a3147fdebd3faf29
Maarten Geerse <mgeerse@gmail.com>
2022-08-17 14:50:23 +0200

    remove console log

--- functions/redirect-url-composer/1.0/index.js
+++ functions/redirect-url-composer/1.0/index.js
@@ -1,7 +1,5 @@
 const redirectUrlComposer = async ({ url, placeholderMapping }) => {
   placeholderMapping.map((placeholder) => {
-    console.log(placeholder.key);
-    console.log(placeholder.value);
     url = url.replace(':' + placeholder.key, placeholder.value);
   });
 
