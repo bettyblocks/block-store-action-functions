240a0f8b78815ed7c2900d815da76c084c6ced96
Maarten Geerse <mgeerse@gmail.com>
2022-08-17 16:15:11 +0200

    feat: add test to export-to-csv

--- test/export-as-csv.test.js
+++ test/export-as-csv.test.js
@@ -1,8 +1,25 @@
 test('Test Export as CSV', async () => {
-  const output = await $app['exportAsCSV 1.0']({
-    collectionInput: [{}, {}],
-    mapping: {},
+  const output = await $app['exportAsCsv 1.0']({
+    collectionInput: {
+      data: [
+        { foo: 'corge', bar: 'qux', baz: 'quuz' },
+        { foo: 'garply', bar: 'waldo', baz: 'fred' },
+      ],
+    },
+    headerMap: [
+      {
+        key: 'foo',
+        value: 'thud',
+      },
+      {
+        key: 'bar',
+        value: 'bar',
+      },
+    ],
+    delimiter: ';',
+    model: { name: 'myModel' },
+    property: [{ name: 'myProperty' }],
   });
 
-  // assert(output, { greet: 'Hello, Bruce' });
+  assert(output, { reference: "Thomas' Awesome File Reference" });
 });
--- test/helpers.js
+++ test/helpers.js
@@ -1,3 +1,9 @@
 const gql = async (query) => {
   return [{ id: 1, name: 'Bruce Wayne' }];
 };
+
+const storeFile = function (modelName, property, {}) {
+  return "Thomas' Awesome File Reference";
+};
+
+const stringToBuffer = () => 'ZWFzdGVyIGVnZyBmb3VuZCE=';
