d990ba33cd29edf8ce50da2b0388661c9d2d95fa
Maarten Geerse <mgeerse@gmail.com>
2022-08-17 14:18:03 +0200

    remove .tmp from commit

--- .tmp/node_modules/app/export-as-csv/1.0/index.js
+++
@@ -1,6 +0,0 @@
-const exportAsCSV = async ({ collectionInput, mapping }) => {
-  console.log(collectionInput);
-  console.log(mapping);
-};
-
-export default exportAsCSV;
--- .tmp/node_modules/app/index.js
+++
@@ -1,9 +0,0 @@
-import { default as exportAsCsv_1_0 } from './export-as-csv/1.0';
-import { default as sayHello_1_0 } from './say-hello/1.0';
-
-const fn = {
-  "exportAsCsv 1.0": exportAsCsv_1_0,
-  "sayHello 1.0": sayHello_1_0,
-};
-
-export default fn;
