30a70996ddd74039b06d38da4b81d81b092512e7
Maarten Geerse <mgeerse@gmail.com>
2022-08-17 15:08:12 +0200

    chore: rename variables

--- functions/export-as-csv/1.0/index.js
+++ functions/export-as-csv/1.0/index.js
@@ -5,7 +5,7 @@ const exportAsCSV = async ({
   model: { name: modelName },
   property: [{ name: propertyName }],
 }) => {
-  const replacer = (value) => (value === null || value === 'null' ? '' : value); // specify how you want to handle null values here
+  const replacer = (value) => (value === null || value === 'null' ? '' : value);
 
   const allowedHeaders = headerMap.map((header) =>
     header.key.toLocaleLowerCase()
@@ -15,24 +15,25 @@ const exportAsCSV = async ({
     headerMap.map((header) => header.value).join(delimiter) + '\r\n',
   ];
 
-  collectionInput.data.map((dataObject) => {
-    let row = '';
-    let i = o;
-    for (const [key, value] of Object.entries(dataObject)) {
-      if (allowedHeaders.includes(key.toLocaleLowerCase()))
-        row += replacer(value);
-      if (i < Object.entries(dataObject).length - 1) row += delimiter; //we dont want the delimiter on the last value
-    }
-    csvTextArray.push(row);
-  });
+  const data = collectionInput.data
+    .map((dataObject) => {
+      let row = '';
+      let i = o;
+      for (const [key, value] of Object.entries(dataObject)) {
+        if (allowedHeaders.includes(key.toLocaleLowerCase()))
+          row += replacer(value);
+        if (i < Object.entries(dataObject).length - 1) row += delimiter;
+      }
+      csvTextArray.push(row);
+    })
+    .join('\r\n');
 
-  console.log(csvTextArray.join('\r\n'));
   return {
     fileReference: await storeFile(modelName, propertyName, {
-      'Content-type': 'text/csv',
+      contentType: 'text/csv',
       extension: '.csv',
       fileName: 'export',
-      fileBuffer: [],
+      fileBuffer: stringToBuffer(data),
     }),
   };
 };
