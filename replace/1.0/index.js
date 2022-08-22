79256dd3797f97aadc6c1b89aa8dd831bd3fef5c
joeri groot <joerigroot@MacBook-Pro-van-joeri-2.local>
2022-08-22 11:48:01 +0200

    feat/split

--- replace/1.0/index.js
+++
@@ -1,8 +0,0 @@
-const replace = async ({ original, whatToReplace, replaceWithWhat }) => {
-	let newString = original.replace(whatToReplace, replaceWithWhat);
-	return {
-		result: newString,
-	};
-};
-
-export default replace;
