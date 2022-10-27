253ba0cf38d09cd2b17e5764fa83489069a56d67
Sam <39275315+degrootsam@users.noreply.github.com>
2022-10-27 14:54:14 +0200

    Delete ThreeJSProject directory

--- ThreeJSProject/objects/plane.js
+++
@@ -1,8 +0,0 @@
-import { PlaneGeometry, MeshBasicMaterial, Mesh , FrontSide} from "three";
-
-const geometry = new PlaneGeometry(100, 100);
-const material = new MeshBasicMaterial({ color: 0xFFFFF, side: FrontSide })
-
-const plane = new Mesh(geometry, material);
-
-export default plane
