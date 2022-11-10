181057755ab266b94d89cecf564170174f24571d
Maarten Geerse <maarten.geerse@bettyblocks.com>
2022-11-10 15:40:57 -0500

    fix: add jest tests

---
+++ __tests__/http2/index.test.js
@@ -0,0 +1,41 @@
+import https2 from '../../functions/http2/1.0';
+
+describe('HTTPS2 Step', () => {
+  test('Makes a succesfull http call.', async () => {
+    expect.assertions(1);
+
+    const request = {
+      url: 'http://example.com',
+      method: 'get',
+      body: '',
+      headers: [
+        { key: 'Content-Type', value: 'application/json; charset=UTF-8' },
+      ],
+      protocol: 'http',
+      variables: [],
+      queryParameters: [{ key: 'name', value: 'foo' }],
+    };
+
+    const { as } = await https2(request);
+
+    expect(as).toBe('return text');
+  });
+  test('Will crash when fetch throws errors.', () => {
+    expect.assertions(1);
+
+    const request = {
+      url: 'http://error.com',
+      method: 'get',
+      body: '',
+      headers: [
+        { key: 'Content-Type', value: 'application/json; charset=UTF-8' },
+      ],
+      protocol: 'http',
+      variables: [],
+      queryParameters: [{ key: 'name', value: 'foo' }],
+    };
+    https2(request).catch(({ message }) => {
+      expect(message).toBe('Something went wrong.');
+    });
+  });
+});
