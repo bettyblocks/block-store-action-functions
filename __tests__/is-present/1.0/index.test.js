8e0a60de9962366813bffdcad5ed5792c5a06d1c
Gijs van Wichen <gijs.van.wichen@bettyblocks.com>
2022-11-11 15:46:44 +0100

    fix lint issue

--- __tests__/is-present/1.0/index.test.js
+++ __tests__/is-present/1.0/index.test.js
@@ -1,12 +1,12 @@
-import isPresent from "../../../functions/is-present/1.0";
+import isPresent from '../../../functions/is-present/1.0';
 
-describe("Check if record is present", () => {
-  test("Check if record is present", async () => {
+describe('Check if record is present', () => {
+  test('Check if record is present', async () => {
     const { result } = await isPresent({ record: { data: { id: 1 } } });
     expect(result).toEqual(true);
   });
 
-  test("Check if record is not present", async () => {
+  test('Check if record is not present', async () => {
     const { result } = await isPresent({ record: { data: null } });
     expect(result).toEqual(false);
   });
