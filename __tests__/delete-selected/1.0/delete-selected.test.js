519b084dd790fe95f8cef4fc75e113d984e8b192
Anton van den Berg <anton.vandenberg@bettyblocks.com>
2022-11-18 10:25:08 +0100

    fix: resolvelinting errors

--- __tests__/delete-selected/1.0/delete-selected.test.js
+++ __tests__/delete-selected/1.0/delete-selected.test.js
@@ -1,25 +1,49 @@
 import deleteSelected from '../../../functions/delete-selected/1.0';
 
-// TO DO: Mock data to check if deleted records match return 
+// TO DO: Mock data to check if deleted records match return
 
 describe('Delete selected', () => {
   test('It deletes records from a model based on an array of ids', async () => {
     const modelName = 'Task';
     const selectedIds = [1, 2, 5];
-    const recordsPresent = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }];
-    const { result } = await deleteSelected({ model: { name: modelName }}, { selectedIds });
+    const recordsPresent = [
+      { id: 1 },
+      { id: 2 },
+      { id: 3 },
+      { id: 4 },
+      { id: 5 },
+      { id: 6 },
+    ];
+    const { result } = await deleteSelected(
+      { model: { name: modelName } },
+      { selectedIds },
+    );
     const recordsDeleted = null;
 
     expect(recordsPresent).not.toBeNull();
-    expect(result).toMatch(`${recordsPresent.length - selectedIds.length} ${modelName} records have been deleted`);
+    expect(result).toMatch(
+      `${
+        recordsPresent.length - selectedIds.length
+      } ${modelName} records have been deleted`,
+    );
     expect(recordsDeleted).toBeNull();
   });
 
   test('Only IDs of existing records are returned in the result and counted', async () => {
     const modelName = 'Task';
     const selectedIds = [1, 2, 100];
-    const recordsPresent = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }];
-    const { result } = await deleteSelected({ model: { name: modelName } }, { selectedIds });
+    const recordsPresent = [
+      { id: 1 },
+      { id: 2 },
+      { id: 3 },
+      { id: 4 },
+      { id: 5 },
+      { id: 6 },
+    ];
+    const { result } = await deleteSelected(
+      { model: { name: modelName } },
+      { selectedIds },
+    );
     const recordsDeleted = null;
 
     expect(recordsPresent).not.toBeNull();
--- functions/delete-selected/1.0/index.js
+++ functions/delete-selected/1.0/index.js
@@ -1,33 +1,43 @@
+/* eslint no-console: ["error", { allow: ["log", "error"] }] */
 const deleteSelected = async ({
-    model: { name: modelName },
-    selectedIds = [],
-    logging,
-  }) => {
-    try {
-      const mutationName = `deleteMany${modelName}`;
-      const mutation = `
+  model: { name: modelName },
+  selectedIds = [],
+  logging,
+}) => {
+  try {
+    const mutationName = `deleteMany${modelName}`;
+    const mutation = `
           mutation {
             ${mutationName}(input: $input) {
               id
             }
           }
         `;
-  
-      const { data, errors } = await gql(mutation, { input: { ids: selectedIds } });
-  
-      if (errors) {
-        throw errors;
-      }
-      logging && console.log({ input: selectedIds, result: data });
-      const resultLength = data && data[`deleteMany${modelName}`] && data[`deleteMany${modelName}`].length;
-  
-      return {
-        result: `${resultLength} ${modelName} records have been deleted`,
-      };
-    } catch (error) {
-      logging && console.error({ error });
-      throw error;
+
+    const { data, errors } = await gql(mutation, {
+      input: { ids: selectedIds },
+    });
+
+    if (errors) {
+      throw errors;
     }
-  };
+    if (logging) {
+      console.log({ input: selectedIds, result: data });
+    }
+    const resultLength =
+      data &&
+      data[`deleteMany${modelName}`] &&
+      data[`deleteMany${modelName}`].length;
+
+    return {
+      result: `${resultLength} ${modelName} records have been deleted`,
+    };
+  } catch (error) {
+    if (logging) {
+      console.error({ error });
+    }
+    throw error;
+  }
+};
 
 export default deleteSelected;
