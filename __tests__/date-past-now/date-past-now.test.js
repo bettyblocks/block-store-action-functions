835e6718e17292d3f77249c9440649589cccda8a
Maarten Geerse <maarten.geerse@bettyblocks.com>
2022-12-01 14:15:17 -0500

    feat: test including date validation

--- __tests__/date-past-now/date-past-now.test.js
+++ __tests__/date-past-now/date-past-now.test.js
@@ -1,12 +1,11 @@
 /* eslint-disable no-new-func */
-import moment from 'moment';
 import dateExpired from '../../functions/date-past-now/1.0';
 
 describe('Check if a date(time) input is past the current date(time)', () => {
-  test('Check if yesterday is before the current date(time)', async () => {
-    const dateTime = moment().subtract(1, 'days');
+  test('Check if 31-12-2021 is past 01-01-2022', async () => {
+    const dateTime = '31-12-2021';
     const timeZone = 'Europe/Amsterdam';
-    const { result } = await dateExpired(
+    const { as } = await dateExpired(
       {
         timeZone,
         dateTime,
@@ -14,14 +13,14 @@ describe('Check if a date(time) input is past the current date(time)', () => {
       new Function(),
     );
 
-    expect(result).not.toBeNull();
-    expect(result).toEqual(true);
+    expect(as).not.toBeNull();
+    expect(as).toEqual(true);
   });
 
-  test('Check if tomorrow is after the current date(time)', async () => {
-    const dateTime = moment().add(1, 'days');
+  test('Check if 02-01-2022 is past 01-01-2022', async () => {
+    const dateTime = '02-01-2022';
     const timeZone = 'Europe/Amsterdam';
-    const { result } = await dateExpired(
+    const { as } = await dateExpired(
       {
         timeZone,
         dateTime,
@@ -29,7 +28,22 @@ describe('Check if a date(time) input is past the current date(time)', () => {
       new Function(),
     );
 
-    expect(result).not.toBeNull();
-    expect(result).toEqual(false);
+    expect(as).not.toBeNull();
+    expect(as).toEqual(true);
+  });
+
+  test('Check if kejsgs is past 01-01-2022', async () => {
+    const dateTime = 'kejsgs';
+    const timeZone = 'Europe/Amsterdam';
+
+    await expect(
+      dateExpired(
+        {
+          timeZone,
+          dateTime,
+        },
+        new Function(),
+      ),
+    ).rejects.toThrow('Date input is invalid');
   });
 });
