9531eff88dbaa6584934953178ddb7a206dc2655
BobHansen75 <bob.hansen@bettyblocks.com>
2022-12-02 13:37:44 +0100

    fix: comments from PR

--- __tests__/date-time-offset/1.0/date-time-offset.test.js
+++ __tests__/date-time-offset/1.0/date-time-offset.test.js
@@ -3,236 +3,196 @@ import dateTimeOffset from '../../../functions/date-time-offset/1.0';
 
 describe('Date Time Offset', () => {
   test('return date time for 1 second added to a new javascript date for UTC+1 zone', async () => {
-    const currentDate = true;
-    const customStartDate = null;
-    const offsetType = 'ss';
-    const offset = '1';
-    const resultType = 'DT';
-    const TimeZoneOffset = '60';
-    const checkDate = addMinutes(new Date(), parseInt(TimeZoneOffset, 10));
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
+    const testData = {
+      currentDate: true,
+      customStartDate: null,
+      offsetType: 'ss',
+      offset: '1',
+      resultType: 'DT',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
 
-    const dateDiff = new Date(result) - checkDate;
+    const dateDiff =
+      new Date(result) -
+      addMinutes(new Date(), parseInt(testData.timeZoneOffset, 10));
     expect(dateDiff).toBeLessThan(1000);
     expect(dateDiff).toBeGreaterThan(0);
   });
 
   test('return 1 second added to a new javascript date for UTC-6 zone', async () => {
-    const currentDate = true;
-    const customStartDate = null;
-    const offsetType = 'ss';
-    const offset = '1';
-    const resultType = 'DT';
-    const TimeZoneOffset = '-360';
-    const checkDate = addMinutes(new Date(), parseInt(TimeZoneOffset, 10));
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
-
+    const testData = {
+      currentDate: true,
+      customStartDate: null,
+      offsetType: 'ss',
+      offset: '1',
+      resultType: 'DT',
+      timeZoneOffset: '-360',
+    };
+    const checkDate = addMinutes(
+      new Date(),
+      parseInt(testData.timeZoneOffset, 10),
+    );
+    const { result } = await dateTimeOffset(testData);
     const dateDiff = new Date(result) - checkDate;
     expect(dateDiff).toBeLessThan(1000);
     expect(dateDiff).toBeGreaterThan(0);
   });
 
   test('return time for 1 second added to 2022-01-01 00:00:00 for UTC+1 zone', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 00:00:00';
-    const offsetType = 'ss';
-    const offset = '1';
-    const resultType = 'T';
-    const TimeZoneOffset = '60';
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 00:00:00',
+      offsetType: 'ss',
+      offset: '1',
+      resultType: 'T',
+      timeZoneOffset: '60',
+    };
 
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
+    const { result } = await dateTimeOffset(testData);
     expect(result).toMatch('00:00:01');
   });
 
   test('return date time for 1 second added to 2022-01-01 00:00:00 for UTC+1 zone', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 00:00:00';
-    const offsetType = 'ss';
-    const offset = '1';
-    const resultType = 'DT';
-    const TimeZoneOffset = '60';
-
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 00:00:00',
+      offsetType: 'ss',
+      offset: '1',
+      resultType: 'DT',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
     expect(result).toMatch('2022-01-01 00:00:01');
   });
 
   test('return time for 2 minutes added to 2022-01-01 00:00:00 for UTC+1 zone', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 00:00:00';
-    const offsetType = 'mm';
-    const offset = '2';
-    const resultType = 'T';
-    const TimeZoneOffset = '60';
-
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 00:00:00',
+      offsetType: 'mm',
+      offset: '2',
+      resultType: 'T',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
     expect(result).toMatch('00:02:00');
   });
 
   test('return time for 1 hour added to 2022-01-01 00:00:00 for UTC+1 zone', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 00:00:00';
-    const offsetType = 'hh';
-    const offset = '1';
-    const resultType = 'T';
-    const TimeZoneOffset = '60';
-
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 00:00:00',
+      offsetType: 'hh',
+      offset: '1',
+      resultType: 'T',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
     expect(result).toMatch('01:00:00');
   });
 
   test('return date for 1 day added to 2022-01-01 00:00:00 for UTC+1 zone', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 00:00:00';
-    const offsetType = 'DD';
-    const offset = '1';
-    const resultType = 'DT';
-    const TimeZoneOffset = '60';
-
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 00:00:00',
+      offsetType: 'DD',
+      offset: '1',
+      resultType: 'DT',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
     expect(result).toMatch('2022-01-02 00:00:00');
   });
 
-  test('return date time for 1 week added to 2022-01-01 00:00:00 for UTC+1 zone', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 05:00:00';
-    const offsetType = 'WW';
-    const offset = '1';
-    const resultType = 'D';
-    const TimeZoneOffset = '60';
-
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
+  test('return date for 1 week added to 2022-01-01 01:00:00 for UTC+1 zone', async () => {
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 01:00:00',
+      offsetType: 'WW',
+      offset: '1',
+      resultType: 'D',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
     expect(result).toMatch('2022-01-08');
   });
 
   test('return date time for 1 month added to 2022-01-01 00:00:00 for UTC+1 zone', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 00:00:00';
-    const offsetType = 'MM';
-    const offset = '1';
-    const resultType = 'DT';
-    const TimeZoneOffset = '60';
-
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 00:00:00',
+      offsetType: 'MM',
+      offset: '1',
+      resultType: 'DT',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
     expect(result).toMatch('2022-02-01 00:00:00');
   });
 
   test('return date time for 1 year added to 2022-01-01 00:00:00 for UTC+1 zone', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 00:00:00';
-    const offsetType = 'YYYY';
-    const offset = '1';
-    const resultType = 'DT';
-    const TimeZoneOffset = '60';
-
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 00:00:00',
+      offsetType: 'YYYY',
+      offset: '1',
+      resultType: 'DT',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
     expect(result).toMatch('2023-01-01 00:00:00');
   });
 
-  test('return Invalid offset type', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 00:00:00';
-    const offsetType = 'YY';
-    const offset = '1';
-    const resultType = 'DT';
-    const TimeZoneOffset = '60';
-
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
-    expect(result).toMatch('Invalid offset type');
+  test('return invalid offset type', async () => {
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 00:00:00',
+      offsetType: 'INCORRECT',
+      offset: '1',
+      resultType: 'DT',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
+    expect(result).toMatch('Incorrect offset type');
   });
 
-  test('return Invalid result type', async () => {
-    const currentDate = false;
-    const customStartDate = '2022-01-01 00:00:00';
-    const offsetType = 'MM';
-    const offset = '1';
-    const resultType = 'AA';
-    const TimeZoneOffset = '60';
+  test('return invalid result type', async () => {
+    const testData = {
+      currentDate: false,
+      customStartDate: '2022-01-01 00:00:00',
+      offsetType: 'MM',
+      offset: '1',
+      resultType: 'INCORRECT',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
+    expect(result).toMatch('Incorrect formatting type');
+  });
 
-    const { result } = await dateTimeOffset({
-      currentDate,
-      customStartDate,
-      offsetType,
-      offset,
-      resultType,
-      TimeZoneOffset,
-    });
-    expect(result).toMatch('Invalid result type');
+  test('return invalid date, based on custom incorrect date', async () => {
+    const testData = {
+      currentDate: false,
+      customStartDate: '2023-31-31 00:00:00',
+      offsetType: 'MM',
+      offset: '1',
+      resultType: 'D',
+      timeZoneOffset: '60',
+    };
+    const { result } = await dateTimeOffset(testData);
+    expect(result).toMatch('Invalid Date');
+  });
+
+  test('return invalid date, based on NO data', async () => {
+    const testData = {
+      currentDate: null,
+      customStartDate: null,
+      offsetType: null,
+      offset: null,
+      resultType: null,
+      timeZoneOffset: null,
+    };
+    const { result } = await dateTimeOffset(testData);
+    expect(result).toMatch('Invalid Date');
   });
 });
--- functions/date-time-offset/1.0/index.js
+++ functions/date-time-offset/1.0/index.js
@@ -1,58 +1,48 @@
-import addSeconds from 'date-fns/addSeconds';
-import addHours from 'date-fns/addHours';
-import addMinutes from 'date-fns/addMinutes';
-import addDays from 'date-fns/addDays';
-import addWeeks from 'date-fns/addWeeks';
-import addMonths from 'date-fns/addMonths';
-import addYears from 'date-fns/addYears';
-import { format, parseISO } from 'date-fns';
+import {
+  addSeconds,
+  addHours,
+  addMinutes,
+  addDays,
+  addWeeks,
+  addMonths,
+  addYears,
+  format,
+  parseISO,
+  isValid,
+} from 'date-fns';
 
-const calculateDateTimeOffset = (startDate, offset, offsetType, resultType) => {
-  let calculatedOffset = 'Invalid Date';
-  if (startDate.toString() !== 'Invalid Date') {
-    switch (offsetType) {
-      case 'ss':
-        calculatedOffset = addSeconds(startDate, offset);
-        break;
-      case 'mm':
-        calculatedOffset = addMinutes(startDate, offset);
-        break;
-      case 'hh':
-        calculatedOffset = addHours(startDate, offset);
-        break;
-      case 'DD':
-        calculatedOffset = addDays(startDate, offset);
-        break;
-      case 'WW':
-        calculatedOffset = addWeeks(startDate, offset);
-        break;
-      case 'MM':
-        calculatedOffset = addMonths(startDate, offset);
-        break;
-      case 'YYYY':
-        calculatedOffset = addYears(startDate, offset);
-        break;
-      default:
-        calculatedOffset = 'Invalid offset type';
-    }
-
-    if (calculatedOffset !== 'Invalid offset type') {
-      switch (resultType) {
-        case 'D':
-          calculatedOffset = calculatedOffset.toISOString().substring(0, 10);
-          break;
-        case 'DT':
-          calculatedOffset = format(calculatedOffset, 'yyyy-MM-dd HH:mm:ss');
-          break;
-        case 'T':
-          calculatedOffset = format(calculatedOffset, 'HH:mm:ss');
-          break;
-        default:
-          calculatedOffset = 'Invalid result type';
-      }
-    }
+const calculateDateTimeOffset = (startDate, offset, offsetType) => {
+  switch (offsetType) {
+    case 'ss':
+      return addSeconds(startDate, offset);
+    case 'mm':
+      return addMinutes(startDate, offset);
+    case 'hh':
+      return addHours(startDate, offset);
+    case 'DD':
+      return addDays(startDate, offset);
+    case 'WW':
+      return addWeeks(startDate, offset);
+    case 'MM':
+      return addMonths(startDate, offset);
+    case 'YYYY':
+      return addYears(startDate, offset);
+    default:
+      return 'Incorrect offset type';
   }
-  return calculatedOffset;
+};
+
+const formatDate = (resultType, calculatedOffset) => {
+  switch (resultType) {
+    case 'D':
+      return calculatedOffset.toISOString().substring(0, 10);
+    case 'DT':
+      return format(calculatedOffset, 'yyyy-MM-dd HH:mm:ss');
+    case 'T':
+      return format(calculatedOffset, 'HH:mm:ss');
+    default:
+      return 'Incorrect formatting type';
+  }
 };
 
 const dateTimeOffset = async ({
@@ -61,22 +51,27 @@ const dateTimeOffset = async ({
   offsetType,
   offset,
   resultType,
-  TimeZoneOffset,
+  timeZoneOffset,
 }) => {
   const startDate = currentDate
     ? // eslint-disable-next-line radix
-      addMinutes(new Date(), parseInt(TimeZoneOffset))
+      addMinutes(new Date(), parseInt(timeZoneOffset))
     : parseISO(customStartDate);
 
-  const offsetResult = calculateDateTimeOffset(
-    startDate,
-    offset,
-    offsetType,
-    resultType,
-  );
-
+  if (!isValid(startDate)) {
+    return {
+      result: 'Invalid Date',
+    };
+  }
+  const offsetResult = calculateDateTimeOffset(startDate, offset, offsetType);
+  if (!isValid(offsetResult)) {
+    return {
+      result: offsetResult,
+    };
+  }
+  const returnDate = formatDate(resultType, offsetResult);
   return {
-    result: offsetResult,
+    result: returnDate,
   };
 };
 
