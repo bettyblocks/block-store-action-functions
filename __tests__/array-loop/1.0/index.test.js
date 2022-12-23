4ae2384a4f02336543e8a1348bb7a3873bca5ae5
Gijs van Wichen <gijs.van.wichen@bettyblocks.com>
2022-12-23 12:49:18 +0100

    Update index.test.js

--- __tests__/array-loop/1.0/index.test.js
+++ __tests__/array-loop/1.0/index.test.js
@@ -1,11 +1,11 @@
 import loop from '../../../functions/arrayLoop/1.0';
 
-describe('Native loop', () => {
-  test('It loops over a number array and execustes child steps', async () => {
+describe('Native array loop', () => {
+  test('It loops over a number array and executes child steps', async () => {
     const array = [1, 2, 3, 4, 5];
     const childSteps = jest.fn();
-
-    await loop({ array }, childSteps);
+    const outputType = 'number';
+    await loop({ array, outputType }, childSteps);
 
     expect(childSteps).toHaveBeenCalledTimes(array.length);
     expect(childSteps).toHaveBeenLastCalledWith({
@@ -14,11 +14,38 @@ describe('Native loop', () => {
     });
   });
 
+  test('It loops over a number array and executes child steps', async () => {
+    const array = [1, 2, 3, 4, 5];
+    const childSteps = jest.fn();
+    const outputType = 'text';
+    await loop({ array, outputType }, childSteps);
+
+    expect(childSteps).toHaveBeenCalledTimes(array.length);
+    expect(childSteps).toHaveBeenLastCalledWith({
+      text: array.slice(-1).pop().toString(),
+      index: array.length - 1,
+    });
+  });
+
+  test('It loops over a text array and executes child steps', async () => {
+    const array = ['text 1', 'text 2', 'text 3', 'text 4', 'text 5'];
+    const childSteps = jest.fn();
+    const outputType = 'text';
+    await loop({ array, outputType }, childSteps);
+
+    expect(childSteps).toHaveBeenCalledTimes(array.length);
+    expect(childSteps).toHaveBeenLastCalledWith({
+      text: array.slice(-1).pop(),
+      index: array.length - 1,
+    });
+  });
+
   test('It handles empty arrays', async () => {
     const array = [];
     const childSteps = jest.fn();
+    const outputType = 'number';
 
-    await loop({ array }, childSteps);
+    await loop({ array, outputType }, childSteps);
 
     expect(childSteps).not.toHaveBeenCalled();
   });
