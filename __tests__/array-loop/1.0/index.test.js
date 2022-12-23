cb3fa1fb8999ac6ed2718f5ad9406739e3a89f66
Gijs van Wichen <gijs.van.wichen@bettyblocks.com>
2022-12-23 11:48:32 +0100

    Updated function + tests

--- __tests__/array-loop/1.0/index.test.js
+++ __tests__/array-loop/1.0/index.test.js
@@ -1,7 +1,7 @@
 import loop from '../../../functions/arrayLoop/1.0';
 
 describe('Native loop', () => {
-  test('It loops over a array and execustes child steps', async () => {
+  test('It loops over a number array and execustes child steps', async () => {
     const array = [1, 2, 3, 4, 5];
     const childSteps = jest.fn();
 
@@ -9,7 +9,7 @@ describe('Native loop', () => {
 
     expect(childSteps).toHaveBeenCalledTimes(array.length);
     expect(childSteps).toHaveBeenLastCalledWith({
-      iterator: array.slice(-1).pop(),
+      number: array.slice(-1).pop(),
       index: array.length - 1,
     });
   });
--- functions/arrayLoop/1.0/index.js
+++ functions/arrayLoop/1.0/index.js
@@ -1,7 +1,13 @@
 /* eslint-disable no-await-in-loop */
-const loop = async ({ array = [] }, steps) => {
+const loop = async ({ array = [], outputType }, steps) => {
   for (let index = 0; index < array.length; index += 1) {
-    await steps({ iterator: array[index], index });
+    await steps({
+      [outputType]:
+        outputType === 'number'
+          ? parseInt(array[index], 10)
+          : String(array[index]),
+      index,
+    });
   }
 };
 
