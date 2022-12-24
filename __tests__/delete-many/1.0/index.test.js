eace8aa5470e37b528508e5e46bdc553e1e14b7e
Rob Dekker <rob-ajax@live.nl>
2022-12-24 14:35:27 +0100

    fix: resolve build error by adding an example test

--- __tests__/delete-many/1.0/index.test.js
+++ __tests__/delete-many/1.0/index.test.js
@@ -1,20 +1,20 @@
-// import deleteMany from '../../../functions/delete-many/1.0';
+import deleteMany from '../../../functions/delete-many/1.0';
 
-// const modelName = 'Task';
+const modelName = 'Task';
 
-// describe('Delete many', () => {
-//   test('It deletes all records from the given collection', async () => {
-//     const collectionInput = [{ id: 1 }, { id: 2 }, { id: 5 }];
-//     const { result } = await deleteMany({
-//       collection: { data: collectionInput, model: { name: modelName } },
-//     });
-//     expect(result).toMatch(`3 ${modelName} records have been deleted`);
-//   });
-//   test('It only deletes existing records and returns the correct result value', async () => {
-//     const collectionInput = [{ id: 6 }, { id: 7 }, { id: 250 }];
-//     const { result } = await deleteMany({
-//       collection: { data: collectionInput, model: { name: modelName } },
-//     });
-//     expect(result).toMatch(`2 ${modelName} records have been deleted`);
-//   });
-// });
+describe('Delete many', () => {
+  test('It deletes all records from the given collection', async () => {
+    const collectionInput = [{ id: 1 }, { id: 2 }, { id: 5 }];
+    const { result } = await deleteMany({
+      collection: { data: collectionInput, model: { name: modelName } },
+    });
+    expect(result).toMatch(`3 ${modelName} records have been deleted`);
+  });
+  //   test('It only deletes existing records and returns the correct result value', async () => {
+  //     const collectionInput = [{ id: 6 }, { id: 7 }, { id: 250 }];
+  //     const { result } = await deleteMany({
+  //       collection: { data: collectionInput, model: { name: modelName } },
+  //     });
+  //     expect(result).toMatch(`2 ${modelName} records have been deleted`);
+  //   });
+});
