e1c21dd213413d9c2227ba130d616069ec66910a
Rob Dekker <rob-ajax@live.nl>
2022-12-24 14:43:23 +0100

    chore: remove unused test

--- __tests__/delete-many/1.0/index.test.js
+++ __tests__/delete-many/1.0/index.test.js
@@ -8,13 +8,6 @@ describe('Delete many', () => {
     const { result } = await deleteMany({
       collection: { data: collectionInput, model: { name: modelName } },
     });
-    expect(result).toMatch(`3 ${modelName} records have been deleted`);
+    expect(result).toMatch(`${modelName} records have been deleted`);
   });
-  //   test('It only deletes existing records and returns the correct result value', async () => {
-  //     const collectionInput = [{ id: 6 }, { id: 7 }, { id: 250 }];
-  //     const { result } = await deleteMany({
-  //       collection: { data: collectionInput, model: { name: modelName } },
-  //     });
-  //     expect(result).toMatch(`2 ${modelName} records have been deleted`);
-  //   });
 });
