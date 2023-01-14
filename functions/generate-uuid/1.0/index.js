58523a6d88c1e358ad38b8174b7d72d99d554398
Theo Hollenberg <theo.hollenberg@bettyblocks.com>
2023-01-14 21:55:09 +0100

    feat/added step and tests

---
+++ functions/generate-uuid/1.0/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-bitwise */
+const generateUuid = async () => ({
+  result: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  }),
+});
+export default generateUuid;
