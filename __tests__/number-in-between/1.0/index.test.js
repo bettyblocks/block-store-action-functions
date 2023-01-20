81d812162c9acd1248b5d1d8144900710f93493d
Peter Beers <peter.beers@bettyblocks.com>
2023-01-20 11:03:37 +0100

    add number in between tests
    tests

---
+++ __tests__/number-in-between/1.0/index.test.js
@@ -0,0 +1,52 @@
+import numberInBetween from '../../../functions/number-in-between/1.0';
+
+const collection = {
+  data: [
+    {
+      id: 1,
+      name: 'Objective achieved',
+      from: '50.00',
+      till: '100.00',
+    },
+    {
+      id: 2,
+      name: 'Objective not achieved',
+      from: '25.00',
+      till: '49.99',
+    },
+    {
+      id: 3,
+      name: 'Objective significantly failed',
+      from: '0.00',
+      till: '24.99',
+    },
+  ],
+};
+
+describe('Number in between', () => {
+  test('it will return a property value from a collection if the number given is in between two other values 1', async () => {
+    const { recordId, name } = await numberInBetween({
+      collection,
+      leftProperty: 'from',
+      rightProperty: 'till',
+      value: '37.65',
+      returnProperty: 'name',
+    });
+
+    expect(recordId).toBe(2);
+    expect(name).toBe('Objective not achieved');
+  });
+
+  test('it will return a property value from a collection if the number given is in between two other values 2', async () => {
+    const { recordId, name } = await numberInBetween({
+      collection,
+      leftProperty: 'from',
+      rightProperty: 'till',
+      value: '0',
+      returnProperty: 'name',
+    });
+
+    expect(recordId).toBe(3);
+    expect(name).toBe('Objective significantly failed');
+  });
+});
