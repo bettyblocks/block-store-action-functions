241795f683d2f6a0a6711bde9687a4ebb9cbe913
Peter Beers <peter.beers@bettyblocks.com>
2023-02-03 16:18:17 +0100

    update number in between
    throw errors when data or results are missing

--- functions/number-in-between/1.0/index.js
+++ functions/number-in-between/1.0/index.js
@@ -7,11 +7,21 @@ const numberInBetween = async ({
 }) => {
   const { data } = collection;
 
+  if (data.length === 0) {
+    throw new Error(
+      'The collection is empty. Please make sure there is data present in the collection.',
+    );
+  }
+
   const result = data.filter(
     (item) =>
       parseFloat(item[leftProperty]) <= parseFloat(value) &&
       parseFloat(item[rightProperty]) >= parseFloat(value),
   );
+
+  if (result.length === 0) {
+    throw new Error('No record found where the value is in between.');
+  }
 
   return {
     recordId: result[0].id,
