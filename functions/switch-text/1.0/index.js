3894c5f70251bdc9738bfe4503d939fb4049dd32
Sam <sammiedegroot@gmail.com>
2023-06-02 12:47:19 +0200

    Updated comments
    They now more closely represent what happens

--- functions/switch-text/1.0/index.js
+++ functions/switch-text/1.0/index.js
@@ -1,8 +1,8 @@
 const switchText = async ({ input, cases, defaultCase = '' }) => {
   // The input is a string value
   // E.g case === [{key:  "firstName", value: "Betty"}]
-  // If the input equals the key.name then return the value
-  // If the input does not equal the key.name then return the input
+  // If the input equals any of the given keys then return the corresponding value
+  // If the input does not equal any of the keys then return the defaultCase
 
   let result = defaultCase;
   cases.forEach((caseItem) => {
