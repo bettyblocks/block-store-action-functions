ab89ef1cfc840bb35c79a93c877be99c0214391a
Bas van Ooyen <bas.vanooyen@bettyblocks.com>
2023-11-09 16:03:54 +0100

    chore: info prompts, formatting and removed a console log I forgot

--- functions/agent/1.0/index.js
+++ functions/agent/1.0/index.js
@@ -1,27 +1,38 @@
-const agent = async ({ prompt, variables, apiKey, maxTokens, temperature, model }) => {
-    const variableMap = variables.reduce(
-      (previousValue, currentValue) => ({
-        ...previousValue,
-        [currentValue.key]: currentValue.value,
-      }),
-      {}
-    );
-  
-    const { result } = await generativeAI({
-      authorization: {
-        apiKey,
+const agent = async ({
+  prompt,
+  variables,
+  apiKey,
+  maxTokens,
+  temperature,
+  model,
+}) => {
+  const variableMap = variables.reduce(
+    (previousValue, currentValue) => ({
+      ...previousValue,
+      [currentValue.key]: currentValue.value,
+    }),
+    {},
+  );
+
+  const { result } = await generativeAI({
+    authorization: {
+      apiKey,
+    },
+    endpoint: 'text-to-text',
+
+    params: {
+      prompt,
+      variables: variableMap,
+      model: {
+        model: model,
+        settings: { maxNewTokens: maxTokens, temperature: temperature / 100 },
       },
-      endpoint: "text-to-text",
+    },
+  });
 
-      params: { prompt, variables: variableMap, model: { model: model, settings: { maxNewTokens: maxTokens, temperature: temperature / 100 } } },
-    });
-  
-    console.log(result);
-
-    return {
-      result,
-    };
+  return {
+    result,
   };
-  
-  export default agent;
-  
+};
+
+export default agent;
