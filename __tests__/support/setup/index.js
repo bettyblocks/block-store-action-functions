32c33a871368fd89bc3ef42718cf3cf726de1f2e
Bas <bas.vanooyen@bettyblocks.com>
2024-01-10 14:28:51 +0100

    chore: tests

--- __tests__/support/setup/index.js
+++ __tests__/support/setup/index.js
@@ -4,6 +4,7 @@ import parseToGqlFragment from '../parse-to-gql-fragment';
 import runAction from '../run-action';
 import parseData from '../parse-data';
 import generativeAI from '../generative-ai';
+import searchCollection from '../search-collection';
 
 global.generatePDF = generatePDF;
 global.gql = gql;
@@ -11,3 +12,4 @@ global.parseToGqlFragment = parseToGqlFragment;
 global.runAction = runAction;
 global.parseData = parseData;
 global.generativeAI = generativeAI;
+global.searchCollection = searchCollection;
