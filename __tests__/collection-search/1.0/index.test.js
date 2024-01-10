da9741520e8c0c2c113ec55ddb5328893d6e22c6
Bas <bas.vanooyen@bettyblocks.com>
2024-01-10 14:29:45 +0100

    chore: formatter

--- __tests__/collection-search/1.0/index.test.js
+++ __tests__/collection-search/1.0/index.test.js
@@ -1,22 +1,26 @@
 import collectionSearch from '../../../functions/collection-search/1.0';
 
 describe('collectionSearch', () => {
-    it('should return an array of matching items', async () => {
-        const collection = [
-            { idx: 1, name: 'John' },
-            { idx: 2, name: 'Jane' },
-            { idx: 3, name: 'John Doe' },
-        ];
-        const searchTerm = 'John';
-        const propertyName = 'name'; // Add this line to define the propertyName variable
+  it('should return an array of matching items', async () => {
+    const collection = [
+      { idx: 1, name: 'John' },
+      { idx: 2, name: 'Jane' },
+      { idx: 3, name: 'John Doe' },
+    ];
+    const searchTerm = 'John';
+    const propertyName = 'name'; // Add this line to define the propertyName variable
 
-        const result = await collectionSearch({ query: searchTerm, collection: collection, property: [{ name: propertyName }] });
-        console.log(result)
-
-        expect(result.result).toEqual([
-            { idx: 1, name: 'John', score: 0.5 },
-            { idx: 3, name: 'John Doe', score: 0.3 },
-            { idx: 2, name: 'Jane', score: 0.1 },
-        ]);
+    const result = await collectionSearch({
+      query: searchTerm,
+      collection: collection,
+      property: [{ name: propertyName }],
     });
+    console.log(result);
+
+    expect(result.result).toEqual([
+      { idx: 1, name: 'John', score: 0.5 },
+      { idx: 3, name: 'John Doe', score: 0.3 },
+      { idx: 2, name: 'Jane', score: 0.1 },
+    ]);
+  });
 });
--- __tests__/support/search-collection/index.js
+++ __tests__/support/search-collection/index.js
@@ -1,9 +1,11 @@
 function searchCollection(collection, query, property, chunk = false) {
-    return {result: [
-        { idx: 1, name: 'John', score: 0.5 },
-        { idx: 3, name: 'John Doe', score: 0.3 },
-        { idx: 2, name: 'Jane', score: 0.1 },
-    ]};
+  return {
+    result: [
+      { idx: 1, name: 'John', score: 0.5 },
+      { idx: 3, name: 'John Doe', score: 0.3 },
+      { idx: 2, name: 'Jane', score: 0.1 },
+    ],
+  };
 }
 
 module.exports = searchCollection;
