3daea582c63ed7a583325f051bc927564820ac1e
Emiel Wit <emiel.wit@bettyblocks.com>
2024-01-18 13:10:37 +0100

    feat: add file property support

--- functions/parse-document/1.0/index.js
+++ functions/parse-document/1.0/index.js
@@ -1,6 +1,11 @@
+const isFileProperty = (value) =>
+  value && typeof value === 'object' && 'url' in value;
+
 const parseDocument = async ({ document, density, forceImage }) => {
+  const url = isFileProperty(document) ? document?.url : document;
+
   const { result } = await documentParser({
-    document,
+    document: url,
     parseOptions: { density, forceImage },
   });
 
