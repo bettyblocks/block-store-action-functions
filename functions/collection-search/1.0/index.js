e7705d4c31377c75012932c538c6ba0545761ac6
Bas <bas.vanooyen@bettyblocks.com>
2024-01-30 10:58:43 +0100

    chore: eslint

--- functions/collection-search/1.0/index.js
+++ functions/collection-search/1.0/index.js
@@ -2,13 +2,13 @@ const collectionSearch = async ({
   query,
   collection,
   threshold,
-  property: [{ name: propertyName }]
+  property: [{ name: propertyName }],
 }) => {
   const { result } = await searchCollection({
     query,
     prefilterThreshold: threshold,
     documents: collection?.data ?? [],
-    fields: [propertyName]
+    fields: [propertyName],
   });
 
   return {
