9e31129bed15a8224a8b66d9beba32db425496cf
Enrique Bos <enriquebos7090@gmail.com>
2025-03-19 11:49:42 +0100

    Logging under advanced and fixed indentation

--- functions/expression/1.3/index.js
+++ functions/expression/1.3/index.js
@@ -1,23 +1,25 @@
-import templayed from "../../utils/templayed";
-import { variableMap } from "../../utils/utilityFuncs";
+import templayed from '../../utils/templayed';
+import { variableMap } from '../../utils/utilityFuncs';
 
-const customExpression = async ({ expression, variables }) => {
-    const parsedVars = variableMap(variables);
-    const template = templayed(expression)(parsedVars);
-    let functionOutput;
+const customExpression = async ({ expression, variables, debugLogging }) => {
+  const parsedVars = variableMap(variables);
+  const template = templayed(expression)(parsedVars);
+  let functionOutput;
+	
+  try {
+    functionOutput = new Function(`return ${template}`)();
+  } catch (error) {
+    const errorMessage = `Error evaluating expression: "${error.message}" (template: ${template} variables: ${JSON.stringify(parsedVars)})`;
 
-    try {
-        functionOutput = new Function(`return ${template}`)();
+    if (debugLogging) {
+      console.error(errorMessage);
     }
-    catch (error) {
-        const errorMessage = `Error evaluating expression: "${error.message}" (template: ${template} variables: ${JSON.stringify(parsedVars)})`;
-        console.error(errorMessage);
-        throw new Error(errorMessage);
-    }
-    
-    return {
-        result: functionOutput,
-    };
+
+    throw new Error(errorMessage);
+  }
+  return {
+    result: functionOutput,
+  };
 };
 
 export default customExpression;
